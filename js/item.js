ac2e01e211eae3d4dc8e161d3527694faca1540b
AndreyHP <andreymarins501@gmail.com>
2025-02-05 21:50:19 -0300

    first commit

--- js/item.js
+++ js/item.js
@@ -2,12 +2,15 @@ const imageContainer = document.getElementById('item-container');
 
 function showItem(){
     const img = document.createElement('img');
+    const p = document.createElement('p');
+    p.textContent = 'Price: $80.99';
     img.src = './img/food.png';
     img.alt = 'Dog food';
     img.id = 'products';
     img.style.width = '300px';
     img.style.height = '300px';
     imageContainer.appendChild(img);
+    imageContainer.appendChild(p);
 }
 
 showItem();
